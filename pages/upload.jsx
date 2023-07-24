30acc990f06d9da473a1c1cfb3b9879e6154c09b
PremSai999 <premsaivarma999@gmail.com>
2023-07-24 13:27:48 +0530

    upload updated

--- pages/upload.jsx
+++ pages/upload.jsx
@@ -2,11 +2,19 @@ import { useState } from 'react';
 import {firebase} from '../lib/firebase';
 import 'firebase/firestore';
 import 'firebase/storage';
+import axios from 'axios';
+
+async function sendData(data) {
+  const response = await axios.post('http://localhost:3000/myRoute', data);
+  const responseData = response.data;
+  // ...
+}
 
 export default function UploadForm() {
   const [file, setFile] = useState(null);
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
   const [artist, setArtist] = useState('');
   const [podcast, setPodcast] = useState(null);
   const [id, setId] = useState("");
@@ -17,44 +25,15 @@ export default function UploadForm() {
   };
 
   const handleUpload = async () => {
-    if (!file) return;
-
-    // Upload file to Firebase Storage
-    const storageRef = firebase.storage().ref();
-    const fileRef = storageRef.child(file.name);
-    const uploadTask = fileRef.put(file);
-
-    // Update progress
-    uploadTask.on(
-      'state_changed',
-      (snapshot) => {
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setProgress(progress);
-      },
-      (error) => {
-        console.log(error);
-      },
-      async () => {
-        // Get download URL
-        const downloadUrl = await fileRef.getDownloadURL();
-
-        // Save podcast information to Firestore
-        const docRef = await firebase.firestore().collection('podcasts').add({
-          title,
-          description,
-          artist,
-          url: downloadUrl
-        });
-
-        setId(docRef.id);
-      }
-    );
+    const data = {title,artist,image:imageUrl,description}
+    const response = await axios.post('http://localhost:5000/add', data);
+    console.log(response);
   };
 
   const handleGetPodcast = async () => {
     if (!id) return;
 
-    // Get podcast information from Firestore
+    // Get podcast information from Firestore (Need to change this to mongodb server)
     const docRef = firebase.firestore().collection('podcasts').doc(id);
     const doc = await docRef.get();
     if (doc.exists) {
@@ -68,6 +47,7 @@ export default function UploadForm() {
       <input type="text" placeholder="Title" onChange={(event) => setTitle(event.target.value)} />
       <input type="text" placeholder="Description" onChange={(event) => setDescription(event.target.value)} />
       <input type="text" placeholder="Artist" onChange={(event) => setArtist(event.target.value)} />
+      <input type="text" placeholder="ImageUrl" onChange={(event) => setImageUrl(event.target.value)} />
       <input type="file" accept="audio/mp4" onChange={handleFileChange} />
       <button onClick={handleUpload}>Upload</button>
       <progress value={progress} max="100" />
